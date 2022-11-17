6bf166df3343f1ca8057318821b96d5cc31ec5ff
Nontagrich <113359860+Nontagrich@users.noreply.github.com>
2022-11-17 21:30:26 +0700

    5

--- 5.cpp
+++ 5.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    
+    int num1,num2,num3;
+    float average;
+
+    cout << "Please Enter 3 Numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    average = (num1 + num2 + num3)/3;
+
+    cout << "Average = " << fixed << setprecision(2) << average;
 }
