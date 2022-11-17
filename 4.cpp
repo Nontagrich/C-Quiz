cfef30247ced9646be51400c8566bbadba8d9f75
Nontagrich <113359860+Nontagrich@users.noreply.github.com>
2022-11-17 21:27:05 +0700

    4

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char first[20],sur[20];
+
+    cout << "Enter First Name: ";
+    cin >> first;
+
+    cout << "Enter Surname: ";
+    cin >> sur;
+
+    cout << "Hello ! " << first << " " << sur;
+}
